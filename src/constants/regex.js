d84350e0bd83031cab838fbeae52597b0d175abf
petatemarvin26 <petatemarvin26@gmail.com>
2021-10-16 14:11:09 +0800

    update constants/regex

--- src/constants/regex.js
+++ src/constants/regex.js
@@ -2,7 +2,7 @@ const NAME_REGEX = /^[a-zA-Z ]*$/;
 const NUMBER_REGEX = /^[0-9]*$/;
 const SMALL_CHAR_REGEX = /(?=.*[a-z])/;
 const CAPITAL_CHAR_REGEX = /(?=.*[A-Z])/;
-const NUMS_REGEX = '/(?=.*[0-9])/';
+const NUMS_REGEX = /(?=.*[0-9])/;
 const SYMBOLS_REGEX = /(?=.*[!@#$%^&*()_=+-/])/;
 const MONEY_REGEX = /^\$?\d+(,\d{3})*(\.\d*)?$/;
 const USERNAME_REGEX = /^[a-zA-Z0-9]*$/;
