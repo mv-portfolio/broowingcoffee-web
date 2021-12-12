a75aaf71061d27c1efbbb8cb9cba7eaed62684ee
Marvin Petate <petatemarvin26@gmail.com>
2021-12-12 18:17:15 +0800

    update

--- src/pages/Dashboard/Statistics/components/Item/index.js
+++ src/pages/Dashboard/Statistics/components/Item/index.js
@@ -1,14 +1,18 @@
-import {Text, View} from 'components';
+import {Separator, Text, View} from 'components';
 import Formatter from 'utils/Formatter';
 import styles from './.module.css';
 
 export default function Item({place, color, product, availed}) {
   return (
     <View style={styles.mainPane}>
-      <Text style={styles.title} defaultStyle={{color}}>{`${
-        place + 1
-      }. ${Formatter.toName(product)}`}</Text>
-      <Text style={styles.value}>{availed}</Text>
+      <View style={styles.leftPane}>
+        <Text style={styles.title}>{`${place + 1}.`}</Text>
+        <Separator horizontal={0.75} />
+        <Text style={styles.title} defaultStyle={{color}}>
+          {Formatter.toName(product)}
+        </Text>
+      </View>
+      <Text style={styles.value} defaultStyle={{color}}>{availed}</Text>
     </View>
   );
 }
