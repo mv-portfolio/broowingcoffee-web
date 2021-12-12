a43cfee1fe3d95126520ae8f277c991a8a5a4234
Marvin Petate <petatemarvin26@gmail.com>
2021-12-12 15:49:33 +0800

    update project structure

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React, {Suspense} from 'react';
 import ReactDom from 'react-dom';
 import Loading from 'pages/Loading';
 import App from 'App';
-import './index.css';
 
 require('dotenv').config();
 
